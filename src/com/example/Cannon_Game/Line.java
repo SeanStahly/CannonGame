1c36b23f848a8e9fae494c1f65c80f3b6c645d06
Sean <seanstahly@gmail.com>
2014-10-01 11:00:13 -0500

    Began cannon game

---
+++ src/com/example/Cannon_Game/Line.java
@@ -0,0 +1,16 @@
+package com.example.Cannon_Game;
+
+import android.graphics.Point;
+
+/**
+ * Created by sean on 10/1/14.
+ */
+public class Line {
+    public Point start;
+    public Point end;
+
+    public Line () {
+        start = new Point(0, 0);
+        end = new Point(0, 0);
+    }
+}
